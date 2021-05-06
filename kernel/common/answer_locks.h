bf311069fe5dc079be30bb4420cf57af19571b36
Icefoxzhx <1104161801@qq.com>
2021-05-06 17:16:48 +0800

    First Encounter

--- kernel/common/answer_locks.h
+++ kernel/common/answer_locks.h
@@ -9,7 +9,8 @@
 int lock_init(struct lock *lock){
     
     /* Your code here */
-    __sync_lock_release(lock->locked);
+    lock->cpuid=-1;
+    lock->locked=0;
     if(nlock >= MAXLOCKS) BUG("Max lock count reached.");
     locks[nlock++] = lock;
     return 0;
@@ -17,20 +18,24 @@ int lock_init(struct lock *lock){
 
 void acquire(struct lock *lock){
     /* Your code here */
-    __sync_lock_test_and_set(lock->locked,1);
+    while(__sync_lock_test_and_set(&(lock->locked),1));
+    lock->cpuid=cpuid();
 }
 
 // Try to acquire the lock once
 // Return 0 if succeed, -1 if failed.
 int try_acquire(struct lock *lock){
     /* Your code here */
-    if(__sync_lock_test_and_set(lock->locked,1)==0) return -1;
+    if(__sync_lock_test_and_set(&(lock->locked),1)) return -1;
+    lock->cpuid=cpuid();
     return 0;
 }
 
 void release(struct lock* lock){
     /* Your code here */
-    __sync_lock_release(lock->locked);
+    if(!holding_lock(lock)) return;
+    lock->cpuid=-1;
+    __sync_lock_release(&(lock->locked));
 }
 
 int is_locked(struct lock* lock){
@@ -40,7 +45,7 @@ int is_locked(struct lock* lock){
 // private for spin lock
 int holding_lock(struct lock* lock){
     /* Your code here */
-    return lock->cpuid==cpuid()?0:-1;
+    return lock->cpuid==cpuid();
 }
 
 #endif  // ACMOS_SPR21_ANSWER_LOCKS_H
